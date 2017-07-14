85b22760f18a637cdddb48b9ce8eb1174e8d172d
yc-lin <linlichangk@gmail.com>
2017-07-14 09:31:49 +0800

    update

--- init.el
+++ init.el
@@ -82,22 +82,66 @@
 ;;    '(("(\\s-*\\(\\_<\\(?:\\sw\\|\\s_\\)+\\)\\_>"
 ;;           1 'font-lock-func-face)))
 
-(defvar yc-a 1 "Initial setting for the `a` global variable.")
 
-(defun my-switch ()
-  "Doc-string for `my-switch` function."
+(defvar ivy-regex-switch-value 1 "Initial setting for the `a` global variable.")
+(defun ivy-regex-switch ()
+  "Doc-string for `ivy-regex-switch` function."
   (interactive)
   (cond
-    ((= yc-a 1)
-     (message "true")
-     (setq yc-a 2))
-    ((= yc-a 2)
-     (message "false")
-     (setq yc-a 1))))
+   ((= ivy-regex-switch-value 1)
+     (message "Select ivy-regex")
+     (setq ivy-re-builders-alist
+           '((swiper . ivy--regex)
+             (t . ivy--regex-plus)))
+     (setq ivy-regex-switch-value 2))
+   ((= ivy-regex-switch-value 2)
+     (message "Select ivy-regex-plus")
+     (setq ivy-re-builders-alist
+           '((swiper . ivy--regex-plus)
+             (t . ivy--regex-plus)))
+     (setq ivy-regex-switch-value 3))
+   ((= ivy-regex-switch-value 3)
+     (message "Select ivy-regex-ignore-order")
+     (setq ivy-re-builders-alist
+           '((swiper . ivy--regex-ignore-order)
+             (t . ivy--regex-plus)))
+     (setq ivy-regex-switch-value 4))
+   ((= ivy-regex-switch-value 4)
+     (message "Select ivy-regex-fuzzy")
+     (setq ivy-re-builders-alist
+           '((swiper . ivy--regex-fuzzy)
+             (t . ivy--regex-plus)))
+     (setq ivy-regex-switch-value 1))
+    ))
+
+(setq yc-ag-base-command '"ag --follow --nocolor --nogroup %s")
+(setq yc-ag-arg-context '"ag --follow --nocolor --nogroup -C 2 %s")
+(setq counsel-ag-base-command yc-ag-base-command)
+
+(defvar yc-counsel-ag-arg 1 "counsel-ag-arg")
+(defun yc-counsel-ag-argument-switch ()
+  "Doc-string for `counsel-ag-argument-switch` function."
+  (interactive)
+  (cond
+   ((= yc-counsel-ag-arg 1)
+     (message "with %s" yc-ag-base-command)
+     (setq counsel-ag-base-command yc-ag-base-command)
+     (setq yc-counsel-ag-arg 2))
+   ((= yc-counsel-ag-arg 2)
+     (message "with %s" yc-ag-arg-context)
+     (setq counsel-ag-base-command yc-ag-arg-context)
+     (setq yc-counsel-ag-arg 1))
+    ))
+
+(setq systemverilog-imenu-generic-expression
+      '(("Class" "^class \\(.+\\)" 1)
+        ))
+
+(add-hook 'verilog-mode-hook (lambda () (setq imenu-generic-expression systemverilog-imenu-generic-expression)))
 
 ;;------------------------------------------------------------------------------
 ;; Configuration
-
+(setq x-select-enable-clipboard t)
 (setq inhibit-startup-screen t)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
@@ -123,8 +167,7 @@
                         '(("\\[=+><?:\\]" 1 'font-lock-function-name-face)))
 
 
-;;(add-hook 'prog-mode-hook #'whitespace-mode)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;;(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 
@@ -169,6 +212,9 @@
           (lambda ()
             (counsel-gtags-mode t)))
 
+(add-to-list 'auto-mode-alist '("\\.svi\\'" . verilog-mode))
+(add-to-list 'auto-mode-alist '("\\.svip\\'" . verilog-mode))
+(add-to-list 'auto-mode-alist '("\\.inc\\'" . verilog-mode))
 
 (setq kill-buffer-query-functions (remq 'process-kill-buffer-query-function
                                         kill-buffer-query-functions))
@@ -179,6 +225,24 @@
 
 (require 'verilog-mode)
 (setq verilog-auto-newline nil)
+  (custom-set-variables
+    '(verilog-align-ifelse t)
+    '(verilog-auto-delete-trailing-whitespace t)
+    '(verilog-auto-inst-param-value t)
+    '(verilog-auto-inst-vector nil)
+    '(verilog-auto-lineup (quote all))
+    '(verilog-auto-newline nil)
+    '(verilog-auto-save-policy nil)
+    '(verilog-auto-template-warn-unused t)
+    '(verilog-case-indent 4)
+    '(verilog-cexp-indent 4)
+    '(verilog-highlight-grouping-keywords t)
+    '(verilog-highlight-modules t)
+    '(verilog-indent-level 4)
+    '(verilog-indent-level-behavioral 4)
+    '(verilog-indent-level-declaration 4)
+    '(verilog-indent-level-module 4)
+    '(verilog-tab-to-comment t))
 
 ;;------------------------------------------------------------------------------
 ;; Packages
@@ -205,11 +269,11 @@
 (require 'ivy)
 (ivy-mode 1)
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ivy-highlight-face ((t (:background "#FFFFFF")))))
+;; custom-set-faces was added by Custom.
+;; If you edit it by hand, you could mess it up, so be careful.
+;; Your init file should contain only one such instance.
+;; If there is more than one, they won't work right.
+'(ivy-highlight-face ((t (:background "#FFFFFF")))))
 
 
 (require 'airline-themes)
@@ -242,7 +306,6 @@
 
 ;; (require 'autopair)
 ;; (autopair-global-mode)
-
 
 (require 'expand-region)
 ;; must setting two variable to enable in evil-modeA
@@ -279,7 +342,14 @@
                                           (forward-line -4))
                       ;; example mapping
                       (define-key evil-motion-state-map (kbd "[") #'evil-yc-jump-up)
-                      (define-key evil-motion-state-map (kbd "]") #'evil-yc-jump-down))
+                      (define-key evil-motion-state-map (kbd "]") #'evil-yc-jump-down)
+                      (evil-ex-define-cmd "Wq" 'evil-save-and-close)
+                      (evil-ex-define-cmd "Wqall" 'evil-save-and-quit)
+                      (evil-ex-define-cmd "Wqa" "wqall")
+                      (evil-ex-define-cmd "W" "write")
+                      (evil-ex-define-cmd "Wall" 'evil-write-all)
+                      (evil-ex-define-cmd "Wa" "wall")
+                      )
 
 (require 'ace-window)
 (setq aw-dispatch-always t)
@@ -341,15 +411,11 @@
 (eval-after-load 'flycheck
                  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
-;;etags
-;;(require 'helm-etags-plus)
-;;(setq tags-table-list '("./TAGS"))
-
-;; (autoload 'turn-on-ctags-auto-update-mode
-;;           "ctags-update" "turn on 'ctags-auto-update-mode'."
-;;           t)
-;;(add-hook 'c-mode-common-hook 'turn-on-ctags-auto-update-mode)
-;;(add-hook 'c++-mode-common-hook 'turn-on-ctags-auto-update-mode)
+(require 'smooth-scrolling)
+(smooth-scrolling-mode 1)
+(setq smooth-scroll-margin 3)
+(setq scroll-setup 1
+      scroll-conservatively 1000)
 
 ;;------------------------------------------------------------------------------
 ;;key binding
@@ -366,48 +432,41 @@
                 'next-multiframe-window)
 (global-set-key (kbd "M-4")
                 'swap-buffers-in-windows)
+(global-set-key (kbd "M-5")
+                'ivy-occur)
 (global-set-key (kbd "M-8")
                 'er/expand-region)
-;; (global-set-key (kbd "M-]")
-;;                 'helm-etags-plus-history-go-back)
+(global-set-key (kbd "M-9")
+                'shrink-window)
 (global-set-key (kbd "M-=")
                 'enlarge-window-horizontally)
 (global-set-key (kbd "M--")
                 'shrink-window-horizontally)
 (global-set-key (kbd "M-0")
                 'enlarge-window)
-(global-set-key (kbd "M-9")
-                'shrink-window)
 (global-set-key (kbd "M-s")
                 'slime-selector)
+(global-set-key (kbd "M-r")
+                'ivy-regex-switch)
 
 (require 'ivy-smex)
 (global-set-key (kbd "M-x")
                 'ivy-smex)
-;;(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
-;;(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 (require 'evil-leader)
 (global-evil-leader-mode)
 
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key
-  ;;"f" 'helm-find
   "f" 'counsel-git
   "F" 'counsel-find-file
-  ;;"F" 'helm-find-files
-  ;;"a" 'helm-do-ag
   "a" 'counsel-ag
-  ;;"A" 'helm-do-ag-project-root
-  "A" 'counsel-git-grep
-  ;;"@" 'helm-imenu
+  "A" 'yc-counsel-ag-argument-switch
   "@" 'counsel-imenu
-  ;;"t" 'helm-etags-plus-select
+  ;; "tf" 'counsle-gtags-go-forward
+  ;; "tb" 'counsle-gtags-go-backward
   "ts" 'counsel-gtags-find-symbol
   "tr" 'counsel-gtags-find-reference
   "td" 'counsel-gtags-find-definition
-  "tp" 'counsel-gtags-go-backward
-  "tn" 'counsel-gtags-go-forward
   "T" 'counsel-find-symbol
   "l" 'list-buffers
   "k" 'kill-buffer
@@ -417,11 +476,14 @@
   "cp" 'evilnc-comment-or-uncomment-paragraphs
   "cr" 'comment-or-uncomment-region
   "cv" 'evilnc-toggle-invert-comment-line-by-line
-  "ss" 'slime-selector)
+  "ss" 'slime-selector
+  "gm" 'ivy-wgrep-chtnge-to-wgrep-mode
+  "gc" 'wgrep-finish-edit)
 
 (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 (require 'yasnippet)
 (yas-global-mode 1)
+
 
 ;;------------------------------------------------------------------------------
 (custom-set-variables
@@ -432,19 +494,18 @@
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
-   (vector "#ffffff" "#ff9da4" "#d1f1a9" "#ffeead" "#bbdaff" "#ebbbff" "#99ffff" "#003f8e"))
+   (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#373b41"))
  '(compilation-message-face (quote default))
  '(custom-safe-themes
    (quote
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "70403e220d6d7100bae7775b3334eddeb340ba9c37f4b39c189c2c29d458543b" default)))
- '(fci-rule-color "#003f8e")
+ '(fci-rule-color "#373b41")
  '(highlight-changes-colors (quote ("#ff8eff" "#ab7eff")))
  '(highlight-tail-colors
    (quote
     (("#424748" 0.0)
      ("#63de5d" 0.2)
      ("#4BBEAE" 0.3)
-     ("#1DB4D0" 0.5)
      ("#9A8F21" 0.6)
      ("#A75B00" 0.7)
      ("#F309DF" 0.85)
@@ -453,30 +514,30 @@
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow wgrep rainbow-mode cycle-themes adaptive-wrap counsel-gtags ivy yasnippet highlight-defined srefactor slime-company slime-theme slime use-package evil-nerd-commenter elisp-format whitespace-cleanup-mode rainbow-delimiters iedit highlight-symbol highlight-quoted highlight-parentheses highlight-operators highlight-numbers grizzl git-gutter git-gutter+ flycheck-irony expand-region evil-visualstar evil-smartparens evil-mc evil-leader evil-anzu company-irony-c-headers company-irony clang-format autopair airline-themes ace-window ace-jump-buffer)))
+    (smooth-scrolling vlf color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow wgrep rainbow-mode cycle-themes adaptive-wrap counsel-gtags ivy yasnippet highlight-defined srefactor slime-company slime-theme slime use-package evil-nerd-commenter elisp-format whitespace-cleanup-mode rainbow-delimiters iedit highlight-symbol highlight-quoted highlight-parentheses highlight-operators highlight-numbers grizzl git-gutter git-gutter+ flycheck-irony expand-region evil-visualstar evil-smartparens evil-mc evil-leader evil-anzu company-irony-c-headers company-irony clang-format autopair airline-themes ace-window ace-jump-buffer)))
  '(pos-tip-background-color "#E6DB74")
  '(pos-tip-foreground-color "#242728")
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
-    ((20 . "#ff9da4")
-     (40 . "#ffc58f")
-     (60 . "#ffeead")
-     (80 . "#d1f1a9")
-     (100 . "#99ffff")
-     (120 . "#bbdaff")
-     (140 . "#ebbbff")
-     (160 . "#ff9da4")
-     (180 . "#ffc58f")
-     (200 . "#ffeead")
-     (220 . "#d1f1a9")
-     (240 . "#99ffff")
-     (260 . "#bbdaff")
-     (280 . "#ebbbff")
-     (300 . "#ff9da4")
-     (320 . "#ffc58f")
-     (340 . "#ffeead")
-     (360 . "#d1f1a9"))))
+    ((20 . "#cc6666")
+     (40 . "#de935f")
+     (60 . "#f0c674")
+     (80 . "#b5bd68")
+     (100 . "#8abeb7")
+     (120 . "#81a2be")
+     (140 . "#b294bb")
+     (160 . "#cc6666")
+     (180 . "#de935f")
+     (200 . "#f0c674")
+     (220 . "#b5bd68")
+     (240 . "#8abeb7")
+     (260 . "#81a2be")
+     (280 . "#b294bb")
+     (300 . "#cc6666")
+     (320 . "#de935f")
+     (340 . "#f0c674")
+     (360 . "#b5bd68"))))
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
    (unspecified "#242728" "#424748" "#F70057" "#ff0066" "#86C30D" "#63de5d" "#BEB244" "#E6DB74" "#40CAE4" "#06d8ff" "#FF61FF" "#ff8eff" "#00b2ac" "#53f2dc" "#f8fbfc" "#ffffff")))
