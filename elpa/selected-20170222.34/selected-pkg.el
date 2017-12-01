6c1d9885713dd720dd0ff88070e9cee687570319
yc-lin <linlichangk@gmail.com>
2017-12-01 14:24:42 +0800

    update

---
+++ elpa/selected-20170222.34/selected-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "selected" "20170222.34" "Keymap for when region is active" 'nil :commit "03edaeac90bc6000d263f03be3d889b4685e1bf7" :url "http://github.com/Kungsgeten/selected.el" :keywords '("convenience"))
