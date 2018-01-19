85de0246a1e43ca51e2fd4c98bc00d8eb66f9aae
yc-lin <linlichangk@gmail.com>
2018-01-19 16:00:17 +0800

    update

---
+++ elpa/slime-company-20180108.426/slime-company-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "slime-company" "20180108.426" "slime completion backend for company mode" '((slime "2.13") (company "0.9.0")) :commit "fb54d166ca3e61e8f82020b7c5cfeafd3f4ad425" :keywords '("convenience" "lisp" "abbrev"))
