49e2435c7939e0d1817d6a3c0459708d6baad49f
yclin <yclin@abc>
2018-04-25 00:57:13 -0700

    update

---
+++ pkg/lsp-clangd.el
@@ -0,0 +1,54 @@
+;;; lsp-clangd.el --- clangd support for lsp-mode -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017 Thomas Brown <tabsoftwareconsulting@gmail.com>
+
+;; Author: Thomas Brown <tabsoftwareconsulting@gmail.com>
+;; Version: 3.0
+;; Package-Requires: ((lsp-mode "3.0") (emacs "24.3"))
+;; Keywords: lsp, clang, clangd, c, c++, objective-c, objective-c++
+;; URL: https://github.com/tee3/lsp-clangd
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'lsp-mode)
+(require 'lsp-common)
+
+(defcustom lsp-clangd-executable
+  "clangd"
+  "The clangd executable."
+  :type '(string)
+  :group 'lsp-mode)
+
+(lsp-define-stdio-client lsp-clangd-c++
+                         "cpp"
+                         (lsp-make-traverser "compile_commands.json")
+                         (list lsp-clangd-executable)
+                         :ignore-regexps
+                         '("^Error -[0-9]+: .+$"))
+
+(lsp-define-stdio-client lsp-clangd-c
+                         "c"
+                         (lsp-make-traverser "compile_commands.json")
+                         (list lsp-clangd-executable)
+                         :ignore-regexps
+                         '("^Error -[0-9]+: .+$"))
+
+(lsp-define-stdio-client lsp-clangd-objc
+                         "objective-c"
+                         (lsp-make-traverser "compile_commands.json")
+                         (list lsp-clangd-executable)
+                         :ignore-regexps
+                         '("^Error -[0-9]+: .+$"))
+
+(lsp-define-stdio-client lsp-clangd-objc++
+                         "objective-cpp"
+                         (lsp-make-traverser "compile_commands.json")
+                         (list lsp-clangd-executable)
+                         :ignore-regexps
+                         '("^Error -[0-9]+: .+$"))
+
+(provide 'lsp-clangd)
+
+;;; lsp-clangd.el ends here
